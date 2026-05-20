3b2bec9ebc97f7828d2571c4f7d1d5db1b935110
mahan2103 <mahan.hs2103@gmail.com>
2026-05-20 09:54:00 +0200

    Create controller.cpp

---
+++ controller.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "controller.h"
+
+using namespace std;
+
+void Controller::processData(float soilMoisture, float temperature, float humidity) {
+    cout << "Soil Moisture: " << soilMoisture << "%\n";
+    cout << "Temperature: " << temperature << " C\n";
+    cout << "Humidity: " << humidity << "%\n";
+
+    if (soilMoisture < 40) {
+        cout << "[ACTION] Irrigation ON\n";
+    } else {
+        cout << "[ACTION] Irrigation OFF\n";
+    }
+}
